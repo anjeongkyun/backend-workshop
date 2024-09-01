623832163820adbb3a996ad518a9384a2e279e13
anjeongkyun <dkswjdrbs12@naver.com>
2024-09-02 00:57:05 +0900

    RedisLock 컴포넌트를 구현한다

---
+++ src/main/kotlin/org/flowcontrol/flowcontroller/service/ratelimit/RedisLock.kt
@@ -0,0 +1,19 @@
+package org.flowcontrol.flowcontroller.service.ratelimit
+
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.stereotype.Component
+import java.util.concurrent.TimeUnit
+
+@Component
+class RedisLock(
+    private val redisTemplate: RedisTemplate<String, String>,
+) {
+    fun acquireLock(
+        lockKey: String,
+        timeout: Long,
+    ): Boolean = redisTemplate.opsForValue().setIfAbsent(lockKey, "locked", timeout, TimeUnit.MILLISECONDS) == true
+
+    fun releaseLock(lockKey: String) {
+        redisTemplate.delete(lockKey)
+    }
+}
