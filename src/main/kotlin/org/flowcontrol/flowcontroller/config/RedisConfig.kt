a5743eb3ba3e879c3b748e9b21c81f19b6302bbb
anjeongkyun <dkswjdrbs12@naver.com>
2024-09-02 00:58:35 +0900

    redisTemplate의 Bean을 등록한다

---
+++ src/main/kotlin/org/flowcontrol/flowcontroller/config/RedisConfig.kt
@@ -0,0 +1,16 @@
+package org.flowcontrol.flowcontroller.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+
+@Configuration
+class RedisConfig {
+    @Bean
+    fun redisTemplate(connectionFactory: RedisConnectionFactory): RedisTemplate<String, String> {
+        val template = RedisTemplate<String, String>()
+        template.connectionFactory = connectionFactory
+        return template
+    }
+}
