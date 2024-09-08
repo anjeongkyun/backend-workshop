ecad0a960c06c6c4f198d08daac062c5ef99a389
anjeongkyun <dkswjdrbs12@naver.com>
2024-09-08 20:38:29 +0900

    Bucket4jRateLimiter API Controller를 구현한다

--- rate-limit/src/main/kotlin/org/flowcontroll/controller/RateLimitTestController.kt
+++ rate-limit/src/main/kotlin/org/flowcontroll/controller/RateLimitTestController.kt
@@ -1,6 +1,7 @@
 package org.flowcontroll.controller
 
-import org.flowcontroll.service.RateLimiter
+import org.flowcontroll.service.Bucket4jRateLimiter
+import org.flowcontroll.service.RedisRateLimiter
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
@@ -9,15 +10,28 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class RateLimitTestController(
-    private val rateLimiter: RateLimiter,
+    private val redisRateLimiter: RedisRateLimiter,
+    private val bucket4jRateLimiter: Bucket4jRateLimiter,
 ) {
-    @PostMapping("/rate-limit-test")
+    @PostMapping("/redis/rate-limit-test")
     fun rateLimitTest(
         @RequestHeader(value = "X-Forwarded-For", required = false) ip: String?,
     ): ResponseEntity<String> {
         val clientIp = ip ?: "unknown"
 
-        return if (rateLimiter.isAllowed(clientIp)) {
+        return if (redisRateLimiter.isAllowed(clientIp)) {
+            ResponseEntity.ok("OK")
+        } else {
+            ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).body("Too many requests. Please try again later.")
+        }
+    }
+
+    @PostMapping("/bucket4j/rate-limit-test")
+    fun getData(
+        @RequestHeader(value = "X-Forwarded-For", required = false) ip: String?,
+    ): ResponseEntity<String> {
+        val clientIp = ip ?: "unknown"
+        return if (bucket4jRateLimiter.isAllowed(clientIp)) {
             ResponseEntity.ok("OK")
         } else {
             ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).body("Too many requests. Please try again later.")
